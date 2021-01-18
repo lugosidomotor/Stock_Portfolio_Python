29cb675eacd4b291e2135a0a929059abd5ca422f
Dömötör D. Lugosi <lugosi.domotor@gmail.com>
2021-01-18 11:03:16 +0100

    Update create_csv.py

--- vps/create_csv.py
+++ vps/create_csv.py
@@ -14,14 +14,16 @@ tickers = pd.read_csv('nasdaq.csv')['Symbol']
 df = pd.DataFrame(columns=tickers)
 
 for symbol in tickers:
-  try:
-    df[symbol] = pdr.get_data_yahoo(symbol, start=five_years_ago, end=today)["Adj Close"]
-    print("Reading: " + symbol)
-  except:
-    print("ERROR TO GET DATA")
+    if "." not in str(symbol):
+        try:
+            df[symbol] = pdr.get_data_yahoo(symbol, start=five_years_ago, end=today)["Adj Close"]
+            print("Reading: " + symbol)
+            print(df[symbol])
+        except:
+            print("ERROR TO GET DATA")
 
 #Drop the rows where at least one element is missing.
-df = df.dropna()
+#df = df.dropna()
 
 print(df)
 
