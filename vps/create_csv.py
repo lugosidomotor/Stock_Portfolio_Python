6e6405e201f41279fdab6dee3a50df81a3d2befa
Dömötör D. Lugosi <lugosi.domotor@gmail.com>
2021-01-18 10:46:56 +0100

    Update create_csv.py

--- vps/create_csv.py
+++ vps/create_csv.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
-import pandas_datareader as web
+import pandas_datareader as pdr
 from datetime import date, timedelta
+import yfinance as yf
 
 today = str(date.today())
-five_years_ago = str(date.today() - timedelta(1825))
+five_years_ago = str(date.today() - timedelta(5))
+yf.pdr_override()
 
 #Create the DataFrame and fill with historical data
 tickers = pd.read_csv('nasdaq.csv')['Symbol']
@@ -12,9 +14,15 @@ tickers = pd.read_csv('nasdaq.csv')['Symbol']
 df = pd.DataFrame(columns=tickers)
 
 for symbol in tickers:
-    df[symbol] = web.DataReader(symbol, "yahoo", five_years_ago, today)["Adj Close"]
+  try:
+    df[symbol] = pdr.get_data_yahoo(symbol, start=five_years_ago, end=today)["Adj Close"]
+    print("Reading: " + symbol)
+  except:
+    print("ERROR TO GET DATA")
 
 #Drop the rows where at least one element is missing.
 df = df.dropna()
 
-df.to_csv('stocks' + today + '.csv', index=False)
+print(df)
+
+df.to_csv('stocks-' + today + '.csv', index=False)
