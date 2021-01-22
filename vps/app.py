f1fc5d9b06382d928d415b0050e28d4048798fde
Dömötör D. Lugosi <lugosi.domotor@gmail.com>
2021-01-22 22:51:07 +0100

    Update app.py

--- vps/app.py
+++ vps/app.py
@@ -19,6 +19,7 @@ st.set_page_config(
 	page_icon="💵",
 	initial_sidebar_state="expanded")
 
+
 st.title("💰🤑💵 Portfólió optimalizáló 💵🤑💰")
 #st.subheader('Gecilassú, de teszi a dolgát... ------^ ')
 
@@ -37,7 +38,9 @@ def get_input():
        st.sidebar.write("⚠️ Túl alacsony összeg")
     elif int(value) <= 0:
        st.sidebar.write("⚠️ Negatív összeg")
+
     return value
+
 
 def get_csv():
     options =  values = ["Revolut", "NASDAQ", "NYSE"]
@@ -52,67 +55,75 @@ def get_csv():
        st.sidebar.write("Még nem működik... 🤡  A Revolut opció eredményét látod most.")
     return value
 
-portfolio_val = int(get_input())
-csv = str(get_csv())
-
-####### LOGIC ######
-
-today = str(date.today())
-
-#Create the DataFrame and fill with historical data
-df = pd.read_csv(csv, header = 0)
-
-df = df.clip(lower=0.1)
-print("NEGATIVE: " + str(df.agg(lambda x: sum(x < 0)).sum()))
-
-df = df.iloc[:, 0:200]
-
-assets = df.columns
-
-#Calculate the expected annualized returns and the annualized ...
-mu = expected_returns.mean_historical_return(df)
-S = risk_models.sample_cov(df)
-
-#Optimize fot the maximal Sharpe ratio
-ef = EfficientFrontier(mu,S)
-
-weights = ef.max_sharpe()
-cleaned_weights = ef.clean_weights()
-ef.portfolio_performance(verbose=True)
-
-#Get the discrete allocation of each share per stock
-latest_prices = get_latest_prices(df)
-weights = cleaned_weights
-da = DiscreteAllocation(weights, latest_prices, total_portfolio_value=portfolio_val)
-allocation, leftover = da.lp_portfolio()
-#print('Discrete allocation: ', allocation)
-#print('Funds Rreaming $', leftover)
-
 #Get the companies name
 def get_company_name(symbol):
   url = 'http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=' + symbol + '&region=1&lang=en'
   result = requests.get(url).json()
   for r in result['ResultSet']['Result']:
     if r['symbol'] == symbol:
-      return r['name']
+        return r['name']
 
-symbols = []
-discrete_allocation_list = []
-company_name = []
-for symbol in allocation:
-  company_name.append(get_company_name(symbol))
-  discrete_allocation_list.append(allocation.get(symbol))
-  #symbols.append(allocation.get(symbol))
+####### LOGIC ######
+
+def create_portfolio():
+    today = str(date.today())
+
+#Create the DataFrame and fill with historical data
+    df = pd.read_csv(csv, header = 0)
+
+    df = df.clip(lower=0.1)
+    print("NEGATIVE: " + str(df.agg(lambda x: sum(x < 0)).sum()))
+
+    df = df.iloc[:, 0:200]
+
+    assets = df.columns
+
+#Calculate the expected annualized returns and the annualized ...
+    mu = expected_returns.mean_historical_return(df)
+    S = risk_models.sample_cov(df)
+
+#Optimize fot the maximal Sharpe ratio
+    ef = EfficientFrontier(mu,S)
+
+    weights = ef.max_sharpe()
+    cleaned_weights = ef.clean_weights()
+    ef.portfolio_performance(verbose=True)
+
+
+#Get the discrete allocation of each share per stock
+    latest_prices = get_latest_prices(df)
+    weights = cleaned_weights
+    da = DiscreteAllocation(weights, latest_prices, total_portfolio_value=portfolio_val)
+    allocation, leftover = da.lp_portfolio()
+#print('Discrete allocation: ', allocation)
+#print('Funds Rreaming $', leftover)
+
+    symbols = []
+    discrete_allocation_list = []
+    company_name = []
+    for symbol in allocation:
+        company_name.append(get_company_name(symbol))
+        discrete_allocation_list.append(allocation.get(symbol))
+
+    #symbols.append(allocation.get(symbol))
   
-portfolio_df = pd.DataFrame(columns=['Cég', 'Szimbólum', 'Részvények_száma'])
+    portfolio_df = pd.DataFrame(columns=['Cég', 'Szimbólum', 'Részvények_száma'])
 
-portfolio_df['Cég'] = company_name
-portfolio_df['Szimbólum'] = allocation
-portfolio_df['Részvények_száma'] = discrete_allocation_list
+    portfolio_df['Cég'] = company_name
+    portfolio_df['Szimbólum'] = allocation
+    portfolio_df['Részvények_száma'] = discrete_allocation_list
 
-above_df = "Várható éves hozam: " +  str(round(ef.portfolio_performance(verbose=True)[0] * 100, 2)) + "%   |  " + "Volatilitás: " +  str(round(ef.portfolio_performance(verbose=True)[1] * 100, 2)) + "%   |  " + "Sharpe-ráta: " +  str(round(ef.portfolio_performance(verbose=True)[2], 3))
 
-st.write("Az első két oszlop a cég nevét és tőzsdei szimbólumát mutatja, a harmadik pedig azt, hogy hány darab részvényt kell belőle venned a kalkuláció szerint")
-st.write(above_df , portfolio_df, 'A végén marad: ' + str(round(leftover, 2)) + "$")
-st.write("Elemzéshez felhasznált részvények listája:")
-st.write(assets)
+    above_df = "Várható éves hozam: " +  str(round(ef.portfolio_performance(verbose=True)[0] * 100, 2)) + "%   |  " + "Volatilitás: " +  str(round(ef.portfolio_performance(verbose=True)[1] * 100, 2)) + "%   |  " + "Sharpe-ráta: " +  str(round(ef.portfolio_performance(verbose=True)[2], 3))
+
+
+    st.write("Az első két oszlop a cég nevét és tőzsdei szimbólumát mutatja, a harmadik pedig azt, hogy hány darab részvényt kell belőle venned a kalkuláció szerint")
+    st.write(above_df , portfolio_df, 'A végén marad: ' + str(round(leftover, 2)) + "$")
+    st.write("Elemzéshez felhasznált részvények listája:")
+    st.write(assets)
+
+portfolio_val = int(get_input())
+csv = str(get_csv())
+
+if st.button('📈 Portfólió készítése'):
+    create_portfolio()
